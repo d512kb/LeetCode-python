1cb87d00e2d02a90ac28250c85697fb1b4e5a0e8
Danil Pinchuk <512kb@proton.me>
2025-05-04 00:32:32 -0400

    1829. Maximum XOR for Each Query

--- main.py
+++ main.py
@@ -6,25 +6,13 @@ from itertools import combinations
 from typing import List
 
 class Solution:
-    def smallestNumber(self, pattern: str) -> str:
-        stack = [1]
-        ans = []
-        nextVal = 0;
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        totalXor = 0
+        maxK = (1 << maximumBit) - 1
+        result = []
 
-        for p in pattern:
-            if p == 'I':
-                nextVal = stack[-1] + 1
+        for n in nums:
+            totalXor ^= n
+            result.append(~totalXor & maxK)
 
-                while len(stack) > 0:
-                    ans.append(stack[-1])
-                    stack.pop()
-
-                stack.append(nextVal)
-            else:
-                stack.append(stack[-1] + 1)
-
-        while len(stack) > 0:
-            ans.append(stack[-1])
-            stack.pop()
-
-        return ''.join(map(lambda x: str(x), ans))
+        return result[::-1]
