37c542ca179321048890b0ddf9c1a69e8882b7af
Danil Pinchuk <512kb@proton.me>
2025-05-05 02:29:56 -0400

    264. Ugly Number II

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
+import itertools
 import operator
 
 from collections import defaultdict
 from functools import reduce
-from itertools import combinations
 from typing import List
 
 class Solution:
-    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
-        totalXor = 0
-        maxK = (1 << maximumBit) - 1
-        result = []
+    def nthUglyNumber(self, n: int) -> int:
+        uglyNumbers = [1]
+        i2 = i3 = i5 = 0
 
-        for n in nums:
-            totalXor ^= n
-            result.append(~totalXor & maxK)
+        for _ in range(1, n):
+            ug1 = uglyNumbers[i2]*2
+            ug2 = uglyNumbers[i3]*3
+            ug3 = uglyNumbers[i5]*5
 
-        return result[::-1]
+            ug = min(ug1, ug2, ug3)
+            uglyNumbers.append(ug)
+
+            if ug == ug1:
+                i2 += 1
+            if ug == ug2:
+                i3 += 1
+            if ug == ug3:
+                i5 += 1
+
+        return uglyNumbers[-1]
