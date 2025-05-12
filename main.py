d46b12b665c5223827cd54de27d39d70e7237110
Danil Pinchuk <512kb@proton.me>
2025-05-11 22:45:02 -0400

    3239. Minimum Number of Flips to Make Binary Grid Palindromic I

--- main.py
+++ main.py
@@ -6,51 +6,21 @@ from functools import reduce
 from typing import List
 
 class Solution:
-    def regionsBySlashes(self, grid: List[str]) -> int:
-        sz = len(grid) + 1
-        parent = [-1] * sz * sz
+    def minFlips(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
 
-        def findParent(n):
-            if parent[n] < 0:
-                return n
+        rowSwaps = 0
+        colSwaps = 0
 
-            parent[n] = findParent(parent[n])
-            return parent[n]
+        for row in range(rows):
+            for col in range(cols // 2):
+                if grid[row][col] != grid[row][cols-col-1]:
+                    rowSwaps += 1
 
-        def join(a, b):
-            a = findParent(a)
-            b = findParent(b)
+        for col in range(cols):
+            for row in range(rows // 2):
+                if grid[row][col] != grid[rows-row-1][col]:
+                    colSwaps += 1
 
-            if a == b:
-                return 1
-
-            if a < b:
-                parent[a] += parent[b]
-                parent[b] = a
-            else:
-                parent[b] += parent[a]
-                parent[a] = b
-
-            return 0
-
-        def getUnionId(row, col):
-            return row * sz + col
-
-        for row in range(sz-1):
-            join(getUnionId(row, 0), getUnionId(row+1, 0))
-            join(getUnionId(row, sz-1), getUnionId(row+1, sz-1))
-
-        for col in range(sz-1):
-            join(getUnionId(0, col), getUnionId(0, col+1))
-            join(getUnionId(sz-1, col), getUnionId(sz-1, col+1))
-
-        ans = 1
-
-        for row in range(sz-1):
-            for col in range(sz-1):
-                if grid[row][col] == '/':
-                    ans += join(getUnionId(row, col+1), getUnionId(row+1, col))
-                elif grid[row][col] == '\\':
-                    ans += join(getUnionId(row, col), getUnionId(row+1, col+1))
-
-        return ans
+        return min(rowSwaps, colSwaps)
