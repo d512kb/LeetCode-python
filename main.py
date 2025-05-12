e41724a6b889bb6c41fcd27214cf5ce1cf65761b
Danil Pinchuk <512kb@proton.me>
2025-05-12 09:16:30 -0400

    2456. Most Popular Video Creator

--- main.py
+++ main.py
@@ -5,44 +5,39 @@ from collections import defaultdict
 from functools import reduce
 from typing import List
 
+from dataclasses import dataclass
+
+@dataclass
+class PopularCreator:
+    totalViews: int = 0
+    topVideoViews: int = 0
+    topVideoId: str = chr(ord('z')+1)
+
 class Solution:
-    def minFlips(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
+    def mostPopularCreator(self, creators: List[str], ids: List[str], views: List[int]) -> List[List[str]]:
+        sz = len(creators)
 
-        ans = 0
+        mostViews = 0
+        popularCreators = defaultdict(PopularCreator)
 
-        # squares can't affect the div by 4 rule so just count ans
-        for row in range(rows // 2):
-            for col in range(cols // 2):
-                ones = grid[row][col] + grid[row][cols-col-1] + grid[rows-row-1][col] + grid[rows-row-1][cols-col-1]
-                ans += min(ones, 4-ones)
+        for i in range(sz):
+            creatorId = creators[i]
 
-        ones = 0
-        oddPairs = 0
+            popularCreators[creatorId].totalViews += views[i]
 
-        if rows % 2 == 1:
-            row = rows // 2
+            if popularCreators[creatorId].topVideoViews < views[i]:
+                popularCreators[creatorId].topVideoViews = views[i]
+                popularCreators[creatorId].topVideoId = ids[i]
+            elif popularCreators[creatorId].topVideoViews == views[i] and ids[i] < popularCreators[creatorId].topVideoId:
+                popularCreators[creatorId].topVideoId = ids[i]
 
-            for col in range(cols // 2):
-                onesPair = grid[row][col] + grid[row][cols-col-1]
-                oddPairs += onesPair % 2
-                ones += onesPair // 2 * 2
+            if popularCreators[creatorId].totalViews > mostViews:
+                mostViews = popularCreators[creatorId].totalViews
 
-        if cols % 2 == 1:
-            col = cols // 2
+        ans = []
 
-            for row in range(rows // 2):
-                onesPair = grid[row][col] + grid[rows-row-1][col]
-                oddPairs += onesPair % 2
-                ones += onesPair // 2 * 2
-
-        if rows % 2 == 1 and cols % 2 == 1:
-            ans += grid[rows // 2][cols // 2]
-
-        ans += oddPairs
-
-        if ones % 4 == 2 and oddPairs == 0:
-            ans += 2
+        for id, creator in popularCreators.items():
+            if creator.totalViews == mostViews:
+                ans.append([id, creator.topVideoId])
 
         return ans
