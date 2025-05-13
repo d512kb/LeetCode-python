abf3b6603ecf964c6b635265538b0c06a44fa435
Danil Pinchuk <512kb@proton.me>
2025-05-13 05:05:44 -0400

    3128. Right Triangles

--- main.py
+++ main.py
@@ -1,47 +1,39 @@
+import copy
 import itertools
 import heapq
 import operator
 
 from collections import defaultdict
+from dataclasses import dataclass
 from functools import reduce
 from typing import List
 
-from dataclasses import dataclass
-
-@dataclass
-class ArrayElement:
-    val: int = 0
-    arrayIndex: int = 0
-
 class Solution:
-    def maxDistance(self, arrays: List[List[int]]) -> int:
-        minElements = [ArrayElement(arrays[0][0], 0), ArrayElement(arrays[1][0], 1)]
-        maxElements = [ArrayElement(arrays[0][-1], 0), ArrayElement(arrays[1][-1], 1)]
+    def numberOfRightTriangles(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
 
-        def sortMins():
-            if minElements[0].val > minElements[1].val:
-                minElements[0], minElements[1] = minElements[1], minElements[0]
+        onesByRow = [0] * rows
+        onesByColumn = [0] * cols
 
-        def sortMaxes():
-            if maxElements[0].val < maxElements[1].val:
-                maxElements[0], maxElements[1] = maxElements[1], maxElements[0]
+        for row in range(rows):
+            onesByRow[row] = sum(grid[row])
 
-        sortMins()
-        sortMaxes()
+        for col in range(cols):
+            ones = 0
+            for row in range(rows):
+                ones += grid[row][col]
 
-        for i in range(2, len(arrays)):
-            if arrays[i][0] < minElements[1].val:
-                minElements[1] = ArrayElement(arrays[i][0], i)
-                sortMins()
+            onesByColumn[col] = ones
 
-            if arrays[i][-1] > maxElements[1].val:
-                maxElements[1] = ArrayElement(arrays[i][-1], i)
-                sortMaxes()
+        ans = 0
 
-        if minElements[0].arrayIndex != maxElements[0].arrayIndex:
-            return abs(minElements[0].val - maxElements[0].val)
+        for row in range(rows):
+            if onesByRow[row] == 0:
+                continue
 
-        firstDiff = abs(minElements[0].val - maxElements[1].val)
-        secondDiff = abs(minElements[1].val - maxElements[0].val)
+            for col in range(cols):
+                if (grid[row][col] == 1):
+                    ans += (onesByRow[row]-1) * (onesByColumn[col]-1)
 
-        return max(firstDiff, secondDiff)
+        return ans
