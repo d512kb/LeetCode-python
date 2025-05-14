75e253a3eaa430c81a7f8939bb4b2285085e3029
Danil Pinchuk <512kb@proton.me>
2025-05-14 19:01:46 -0400

    2461. Maximum Sum of Distinct Subarrays With Length K

--- main.py
+++ main.py
@@ -9,18 +9,37 @@ from functools import reduce
 from typing import List
 
 class Solution:
-    def resultsArray(self, nums: List[int], k: int) -> List[int]:
-        sz = len(nums)
-        conseq = 1
-        ans = []
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        ans = 0
+        subarr = defaultdict(int)
+        subarrSum = 0
+        duplicates = 0
 
-        for i in range(sz):
-            if i > 0 and nums[i]-nums[i-1] == 1:
-                conseq += 1
-            else:
-                conseq = 1
+        for i in range(k):
+            subarrSum += nums[i]
+            subarr[nums[i]] += 1
 
-            if i+1 >= k:
-                ans.append(nums[i] if conseq >= k else -1)
+            if subarr[nums[i]] == 2:
+                duplicates += 1
+
+        if duplicates == 0:
+            ans = subarrSum
+
+        for i in range(k, len(nums)):
+            aNum = nums[i-k]
+            bNum = nums[i]
+
+            subarrSum -= aNum
+            subarr[aNum] -= 1
+            if subarr[aNum] == 1:
+                duplicates -= 1
+
+            subarrSum += bNum
+            subarr[bNum] += 1
+            if subarr[bNum] == 2:
+                duplicates += 1
+
+            if duplicates == 0:
+                ans = max(ans, subarrSum)
 
         return ans
