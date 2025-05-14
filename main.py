efd13c8702196eb9887499b9e108d582955f6a74
Danil Pinchuk <512kb@proton.me>
2025-05-14 11:45:46 -0400

    390. Elimination Game

--- main.py
+++ main.py
@@ -9,22 +9,17 @@ from functools import reduce
 from typing import List
 
 class Solution:
-    def lexicalOrder(self, n: int) -> List[int]:
-        ans = []
-        current = 1
+    def lastRemaining(self, n: int) -> int:
+        left = 1
+        step = 1
+        ltr = True
 
-        for _ in range(n):
-            # Add the current number to the result list
-            ans.append(current)
+        while n > 1:
+            if ltr or n % 2 == 1:
+                left += step
 
-            # If multiplying by 10 is valid, move to the next lexicographical branch
-            if current * 10 <= n:
-                current *= 10
-            else:
-                # Otherwise, backtrack to find the next valid number
-                while current % 10 == 9 or current + 1 > n:
-                    current //= 10  # Move up one level in the lexicographical tree
-                current += 1  # Move to the next number
+            n >>= 1
+            step <<= 1
+            ltr = not ltr
 
-        # Return the result list
-        return ans
+        return left
