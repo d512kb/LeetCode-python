88a9a850d83f27d3a645791fa534db581eee9fca
Danil Pinchuk <512kb@proton.me>
2025-05-16 19:23:57 -0400

    1685. Sum of Absolute Differences in a Sorted Array

--- main.py
+++ main.py
@@ -9,37 +9,15 @@ from functools import reduce
 from typing import List
 
 class Solution:
-    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        ans = 0
-        subarr = defaultdict(int)
-        subarrSum = 0
-        duplicates = 0
+    def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
+        sz = len(nums)
+        leftSum = 0
+        rightSum = sum(nums)
+        ans = [0] * sz
 
-        for i in range(k):
-            subarrSum += nums[i]
-            subarr[nums[i]] += 1
-
-            if subarr[nums[i]] == 2:
-                duplicates += 1
-
-        if duplicates == 0:
-            ans = subarrSum
-
-        for i in range(k, len(nums)):
-            aNum = nums[i-k]
-            bNum = nums[i]
-
-            subarrSum -= aNum
-            subarr[aNum] -= 1
-            if subarr[aNum] == 1:
-                duplicates -= 1
-
-            subarrSum += bNum
-            subarr[bNum] += 1
-            if subarr[bNum] == 2:
-                duplicates += 1
-
-            if duplicates == 0:
-                ans = max(ans, subarrSum)
+        for index, num in enumerate(nums):
+            rightSum -= num
+            ans[index] = (num * index - leftSum) + (rightSum - num * (sz-index-1))
+            leftSum += num
 
         return ans
